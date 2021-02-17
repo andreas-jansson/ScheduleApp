e3f2ead06d857c87cfadee734bd8e1bcf2befd7b
andreas-jansson <andreas.jansson@protonmail.com>
2021-02-17 23:57:25 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,209 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View , scrollView,  RefreshControl, SafeAreaView, popOverView } from 'react-native';
+import HTMLView from 'react-native-htmlview';
+import DOMParser from 'react-native-html-parser';
+
+
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Uppdaterad');
+
+  const test1 = (string) =>{
+    return string + '!';
+  };
+
+  const test2 = () =>{
+    return 'Hej';
+  }
+
+  console.log(test1(test2()));
+
+  console.log('Trying...');
+  /*
+  var param = 'startDatum=idag&slutDatum=2021-02-19&sprak=SV&sokMedAND=true&forklaringar=true&resurser=p.H%C3%B6gskoleingenj%C3%B6r+-+Datateknik+%C3%A5k+2-'
+  fetch('https://kronox.oru.se/setup/jsp/Schema.jsp?' + param)
+  .then(response => response.text())
+  .then(data => {console.log(data);})
+  console.log('testar')
+  */
+
+ const getData = () => {
+  var param = 'startDatum=idag&slutDatum=2021-02-19&sprak=SV&sokMedAND=true&forklaringar=true&resurser=p.H%C3%B6gskoleingenj%C3%B6r+-+Datateknik+%C3%A5k+2-';
+  return fetch('https://kronox.oru.se/setup/jsp/Schema.jsp?' + param)
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(typeof(data))
+      return data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+var xdd = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Schema</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='css/style-schema.css') }}">
+</head>
+
+<body>
+<div class="header">
+<h1>Vecka 7</h1>
+</div>
+<table>
+    <tbody>
+<tr class="row-1"><td></td><td>Tis</td><td>9 Feb</td><td>08:15-10:00</td><td>Datorteknik</td><td>tsv</td><td>Digital undervisning</td><td></td><td>Datorteknik Blackboard Collaborate</td><td>2020-11-09</td></tr><tr class="row-0"><td></td><td></td><td></td><td>10:15-12:00</td><td>Artificiell intelligens</td><td>asaffio</td><td>Digital undervisning</td><td></td><td>AI</td><td>2020-11-17</td></tr><tr class="row-1"><td></td><td></td><td></td><td>13:15-17:00</td><td>Datorteknik</td><td>tsv</td><td>T004</td><td></td><td>Datorteknik - Övning grupp 1</td><td>2020-11-09</td></tr><tr class="row-0"><td></td><td>Ons</td><td>10 Feb</td><td>08:15-10:00</td><td>Datorteknik</td><td>tsv</td><td>Digital undervisning</td><td></td><td>Datorteknik Blackboard Collaborate</td><td>2020-11-09</td></tr><tr class="row-1"><td></td><td></td><td></td><td>13:15-17:00</td><td>Datorteknik</td><td>tsv</td><td>T004</td><td></td><td>Datorteknik - Övning grupp 2</td><td>2020-11-09</td></tr><tr class="row-0"><td></td><td>Tor</td><td>11 Feb</td><td>15:15-17:00</td><td>Artificiell intelligens</td><td>asaffio</td><td>Digital undervisning</td><td></td><td>AI</td><td>2020-11-17</td></tr><tr class="row-1"><td></td><td>Fre</td><td>12 Feb</td><td>08:15-12:00</td><td>Artificiell intelligens</td><td>nan</td><td>T004</td><td></td><td>AI - Övning grupp 1</td><td>2020-11-30</td></tr><tr class="row-0"><td></td><td></td><td></td><td>13:15-17:00</td><td>Artificiell intelligens</td><td>nan</td><td>T004</td><td></td><td>AI - Övning grupp 1</td><td>2020-11-30</td>
+        </tbody>
+    </table>
+ </body>
+</html>`
+
+const parseData = (inputstring) => {
+  console.log('parseData');
+  console.log(inputstring);
+  const html = inputstring;    
+  const parser = new DOMParser.DOMParser();
+  const parsed = parser.parseFromString(html, 'text/html');
+  console.log(parsed)
+  return parsed;
+};
+
+parseData(xdd);
+
+
+const test = () => {
+  var teststr = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <title>Schema</title>
+      <link rel="stylesheet" href="{{ url_for('static', filename='css/style-schema.css') }}">
+  </head>
+  
+  <body>
+  <div class="header">
+  <h1>Vecka 7</h1>
+  </div>
+  <table>
+      <tbody>
+  <tr class="row-1"><td></td><td>Tis</td><td>9 Feb</td><td>08:15-10:00</td><td>Datorteknik</td><td>tsv</td><td>Digital undervisning</td><td></td><td>Datorteknik Blackboard Collaborate</td><td>2020-11-09</td></tr><tr class="row-0"><td></td><td></td><td></td><td>10:15-12:00</td><td>Artificiell intelligens</td><td>asaffio</td><td>Digital undervisning</td><td></td><td>AI</td><td>2020-11-17</td></tr><tr class="row-1"><td></td><td></td><td></td><td>13:15-17:00</td><td>Datorteknik</td><td>tsv</td><td>T004</td><td></td><td>Datorteknik - Övning grupp 1</td><td>2020-11-09</td></tr><tr class="row-0"><td></td><td>Ons</td><td>10 Feb</td><td>08:15-10:00</td><td>Datorteknik</td><td>tsv</td><td>Digital undervisning</td><td></td><td>Datorteknik Blackboard Collaborate</td><td>2020-11-09</td></tr><tr class="row-1"><td></td><td></td><td></td><td>13:15-17:00</td><td>Datorteknik</td><td>tsv</td><td>T004</td><td></td><td>Datorteknik - Övning grupp 2</td><td>2020-11-09</td></tr><tr class="row-0"><td></td><td>Tor</td><td>11 Feb</td><td>15:15-17:00</td><td>Artificiell intelligens</td><td>asaffio</td><td>Digital undervisning</td><td></td><td>AI</td><td>2020-11-17</td></tr><tr class="row-1"><td></td><td>Fre</td><td>12 Feb</td><td>08:15-12:00</td><td>Artificiell intelligens</td><td>nan</td><td>T004</td><td></td><td>AI - Övning grupp 1</td><td>2020-11-30</td></tr><tr class="row-0"><td></td><td></td><td></td><td>13:15-17:00</td><td>Artificiell intelligens</td><td>nan</td><td>T004</td><td></td><td>AI - Övning grupp 1</td><td>2020-11-30</td>
+          </tbody>
+      </table>
+   </body>
+  </html>
+  `
+
+  return teststr
+}
+
+//var xd = '';
+//xd = parseData(getData());
+//console.log(xd);
+
+
   return (
+    //<SafeAreaView style={styles.safeContainer}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Mitt Schema</Text>
+      </View>
+
+      <View style={styles.tableContainer}>
+        <Text style={styles.tableTitle}>Vecka: 7</Text>
+
+        <HTMLView value={getData()} styleSheet={styles.table} />
+      </View>
+
+      <View>
+        <Text>{outputText}</Text>
+        <View style={styles.button}>
+          <Button title="Program" onPress={() => setOutputText('Uppdaterat!')} />
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
     </View>
+    //</SafeAreaView>
   );
 }
 
+
+
+//whole screen
 const styles = StyleSheet.create({
-  container: {
+  safeContainer: {
+    flex:1,
+    backgroundColor: 'white'
+  },
+
+  scrollView: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#343536',
+    height:'100%',
+    borderColor: 'black',
+    borderWidth: 2
+  },
+
+  header: {
+    flex:1,
+    color: 'white',
+    height:'10%',
+    paddingTop:4,
+    justifyContent: 'center',
+  //  borderColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'center',
+//    borderWidth: 2
+  },
+
+  tableContainer: {
+    flex:6,
+    backgroundColor: '#c0ede5',
+    height: '80%',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+
+  headerTitle: {
+    color: 'white', 
+    height: 'auto',
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: 50,
+    paddingTop:15
+//  borderColor: 'blue',
+//    borderWidth: 2
+  },
+
+  tableTitle: {
+    color: 'white', 
+    height: 'auto',
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingTop: 10,
+    textDecorationLine: 'underline',
+    alignSelf:'center'
+
+  },
+
+  button: {
+    alignSelf:'center',
+    marginBottom: 100
+  }
+
 });
+
